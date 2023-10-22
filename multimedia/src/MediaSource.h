b20d9e25135192fc20cb855c1eac34b9a14b2ce5
Florian Weßel <florianwessel@gmx.net>
2023-10-22 10:08:03 +0200

    multimedia: Introduce provider base class

---
+++ multimedia/src/MediaSource.h
@@ -0,0 +1,18 @@
+// SPDX-FileCopyrightText: 2021 - 2023 Florian Weßel <florianwessel@gmx.net>
+// Copyright 2020 Florian Weßel <florianwessel@gmx.net>.
+//
+// SPDX-License-Identifier: LGPL-2.1-or-later
+#pragma once
+
+namespace Multimedia
+{
+
+/**
+ * Base class for every media source.
+ * Each MediaSource shall have a name, optional icon and shall provide an MediaItemModel.
+ */
+class MediaSource
+{
+};
+
+} // namespace Multimedia
---
+++ multimedia/test/ProviderShould.cpp
@@ -0,0 +1,54 @@
+// SPDX-FileCopyrightText: 2021 - 2023 Florian Weßel <florianwessel@gmx.net>
+// Copyright 2020 Florian Weßel <florianwessel@gmx.net>.
+//
+// SPDX-License-Identifier: LGPL-2.1-or-later
+#include "ProviderShould.h"
+#include "Provider.h"
+#include <QTest>
+
+namespace Multimedia
+{
+namespace
+{
+
+class TestProvider : public Provider
+{
+    Q_OBJECT
+public:
+    TestProvider()
+        : Provider(QStringLiteral("TestProvider"),
+                   ProviderVersion{.major = quint32{1}, .minor = quint32{2}, .patch = quint32{3}})
+    {
+    }
+};
+
+} // namespace
+
+ProviderShould::~ProviderShould() = default;
+
+void ProviderShould::give_the_name_of_the_provider()
+{
+    const auto provider = TestProvider{};
+    const auto expName = QStringLiteral("TestProvider");
+    const auto name = provider.name();
+    QVERIFY2(name == expName,
+             QStringLiteral("The provider name %1 doesn't match the expected %2 one").arg(name, expName).toLocal8Bit());
+}
+
+void ProviderShould::give_the_version_of_the_provider()
+{
+    const auto provider = TestProvider{};
+    const auto expVersion = ProviderVersion{.major = quint32{1}, .minor = quint32{2}, .patch = quint32{3}};
+    const auto version = provider.version();
+
+    QVERIFY2(version == expVersion,
+             QStringLiteral("The provider version %1 doesn't match the expected %2 one")
+                 .arg(version.toString(), expVersion.toString())
+                 .toLocal8Bit());
+}
+
+} // namespace Multimedia
+
+QTEST_MAIN(Multimedia::ProviderShould);
+
+#include "ProviderShould.moc"
