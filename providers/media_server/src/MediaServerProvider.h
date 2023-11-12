1c188fe6dda10763c7a007872678e63562606376
Florian Weßel <florianwessel@gmx.net>
2023-11-12 19:06:15 +0100

    providers: Add public API documentation for the MediaServerProvider

--- providers/media_server/src/MediaServerProvider.h
+++ providers/media_server/src/MediaServerProvider.h
@@ -13,22 +13,45 @@
 namespace Provider
 {
 
+/**
+ * MediaServerProvider searches the local Network for MediaServers and provides the
+ * access to them by @ref Multimedia::MediaSource interface.
+ */
 class BLABBYMEDIASERVERPROVIDER_EXPORT MediaServerProvider : public Multimedia::Provider
 {
     Q_OBJECT
     Q_INTERFACES(Multimedia::Provider)
     Q_PLUGIN_METADATA(IID "org.blabby.Provider")
 public:
+    /**
+     * Creates an instance of the media server provider
+     * @param serviceProviderFab The factory to create the service provider. Default @ref UPnPAV::ServiceProviderFactory
+     * @param mediaServerFab The factory that shall be used when a creating MediaServer
+     */
     MediaServerProvider(
         std::unique_ptr<UPnPAV::IServiceProviderFactory> serviceProviderFab =
             std::make_unique<UPnPAV::ServiceProviderFactory>(),
         std::unique_ptr<UPnPAV::MediaServerFactory> mediaServerFab = std::make_unique<UPnPAV::MediaServerFactory>());
+
+    /**
+     * Destructor
+     */
     ~MediaServerProvider() override;
 
+    /**
+     * Disabled copy and move operators
+     */
     Q_DISABLE_COPY_MOVE(MediaServerProvider)
 
+    /**
+     * Basic init of the MediaServerProvider
+     * @return always true.
+     */
     bool init() noexcept override;
 
+    /**
+     * @copydoc Multimedia::MediaSource::sources
+     */
     QVector<std::shared_ptr<Multimedia::MediaSource>> sources() const noexcept override;
 
 private Q_SLOTS:
