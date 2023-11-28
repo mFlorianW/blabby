a1952c89468b7ea5e9785067b99945e303439ec3
Florian Weßel <florianwessel@gmx.net>
2023-11-28 19:30:15 +0100

    shell: Add MediaItem to TestDouble

--- multimedia_test_helper/src/TestMediaSource.cpp
+++ multimedia_test_helper/src/TestMediaSource.cpp
@@ -10,6 +10,11 @@ namespace Multimedia::TestHelper
 TestMediaSource::TestMediaSource(QString name, QString iconUrl)
     : Multimedia::MediaSource{std::move(name), std::move(iconUrl)}
 {
+    mMediaItems.emplace_back(Multimedia::MediaItem{Multimedia::MediaItemType::Playable, QStringLiteral("MediaItem1")});
+    mMediaItems.emplace_back(Multimedia::MediaItem{Multimedia::MediaItemType::Playable, QStringLiteral("MediaItem2")});
+    mMediaItems.emplace_back(Multimedia::MediaItem{Multimedia::MediaItemType::Container, QStringLiteral("Container1")});
+    mMediaItems.emplace_back(Multimedia::MediaItem{Multimedia::MediaItemType::Playable, QStringLiteral("MediaItem3")});
+    mMediaItems.emplace_back(Multimedia::MediaItem{Multimedia::MediaItemType::Playable, QStringLiteral("MediaItem4")});
 }
 
 TestMediaSource::~TestMediaSource() = default;
