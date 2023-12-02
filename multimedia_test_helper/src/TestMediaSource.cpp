4832b12bb4c88515b39aad86695216cf43ae909f
Florian Weßel <florianwessel@gmx.net>
2023-12-02 15:10:37 +0100

    multimedia_test_helper: Implement basic MediaSource navigation

--- multimedia_test_helper/src/TestMediaSource.cpp
+++ multimedia_test_helper/src/TestMediaSource.cpp
@@ -12,11 +12,38 @@ TestMediaSource::TestMediaSource(QString name, QString iconUrl)
 {
     mMediaItems.emplace_back(Multimedia::MediaItem{Multimedia::MediaItemType::Playable, QStringLiteral("MediaItem1")});
     mMediaItems.emplace_back(Multimedia::MediaItem{Multimedia::MediaItemType::Playable, QStringLiteral("MediaItem2")});
-    mMediaItems.emplace_back(Multimedia::MediaItem{Multimedia::MediaItemType::Container, QStringLiteral("Container1")});
+    mMediaItems.emplace_back(Multimedia::MediaItem{Multimedia::MediaItemType::Container,
+                                                   QStringLiteral("Container1"),
+                                                   QString(""),
+                                                   QString(""),
+                                                   QStringLiteral("1")});
     mMediaItems.emplace_back(Multimedia::MediaItem{Multimedia::MediaItemType::Playable, QStringLiteral("MediaItem3")});
     mMediaItems.emplace_back(Multimedia::MediaItem{Multimedia::MediaItemType::Playable, QStringLiteral("MediaItem4")});
+    mItems.insert(QStringLiteral("0"), mMediaItems);
+    mItems.insert(QStringLiteral("1"),
+                  {Multimedia::MediaItem{Multimedia::MediaItemType::Playable, QStringLiteral("MediaItem3")},
+                   Multimedia::MediaItem{Multimedia::MediaItemType::Playable, QStringLiteral("MediaItem4")}});
 }
 
 TestMediaSource::~TestMediaSource() = default;
+
+void TestMediaSource::navigateTo(QString const &path) noexcept
+{
+    mLastNavigationPath = path;
+    if (path == QStringLiteral("0"))
+    {
+        mMediaItems = mItems[QStringLiteral("0")];
+    }
+    else if (path == QStringLiteral("1"))
+    {
+        mMediaItems = mItems[QStringLiteral("1")];
+    }
+    Q_EMIT navigationFinished(path);
+}
+
+const QString &TestMediaSource::lastNavigationPath() const noexcept
+{
+    return mLastNavigationPath;
+}
 
 } // namespace Multimedia::TestHelper
