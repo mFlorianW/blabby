7504afa296adaec831decddcfc2294d72591c81c
Florian Weßel <florianwessel@gmx.net>
2023-12-11 22:24:39 +0100

    upnpav: Introduce MediaRenderer factory

--- upnpav/src/MediaRenderer.hpp
+++ upnpav/src/MediaRenderer.hpp
@@ -9,6 +9,9 @@
 
 namespace UPnPAV
 {
+/**
+ * The MediaRenderer is facade for UPnPAV MediaRenderer device.
+ */
 class BLABBYUPNPAV_EXPORT MediaRenderer : public MediaDevice
 {
 public:
@@ -22,6 +25,52 @@ public:
      *         required functions and state variables.
      */
     MediaRenderer(DeviceDescription desc, QSharedPointer<SoapMessageTransmitter> msgTransmitter);
+};
+
+/**
+ * MediaRenderer simplifies the creation of the MediaRenderer and makes it possible
+ * to replace real devices with e.g. test dummies.
+ */
+class BLABBYUPNPAV_EXPORT MediaRendererFactory
+{
+public:
+    /**
+     * Default constructor
+     */
+    MediaRendererFactory() = default;
+
+    /**
+     * Default virtual destructor
+     */
+    virtual ~MediaRendererFactory() = default;
+
+    /**
+     * Copy constructor
+     */
+    MediaRendererFactory(MediaRendererFactory const &other) = default;
+
+    /**
+     * Copy operator
+     */
+    MediaRendererFactory &operator=(MediaRendererFactory const &other) = default;
+
+    /**
+     * Move constructor
+     */
+    MediaRendererFactory(MediaRendererFactory &&other) = default;
+
+    /**
+     * Move operator
+     */
+    MediaRendererFactory &operator=(MediaRendererFactory &&other) = default;
+
+    /**
+     * Factory method that creates tries to create a @ref UPnPAV::MediaRenderer instance.
+     * @throws InvalidDeviceDescription when the DeviceDescription doesn't contain the
+     *         required functions and state variables.
+     * @return The freestore allocated @ref UPnPAV::MediaRenderer
+     */
+    std::unique_ptr<MediaRenderer> create(DeviceDescription const &desc);
 };
 
 } // namespace UPnPAV
