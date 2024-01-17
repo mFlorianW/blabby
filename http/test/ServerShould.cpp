30f8d7fec2a2951a246d3769a1fc34f53548adbf
Florian Weßel <florianwessel@gmx.net>
2024-01-17 20:51:42 +0100

    http: Introduce http component

---
+++ http/test/ServerShould.cpp
@@ -0,0 +1,15 @@
+// SPDX-FileCopyrightText: 2024 All contributors
+//
+// SPDX-License-Identifier: LGPL-2.1-or-later
+
+#include "ServerShould.hpp"
+#include <QTest>
+
+namespace Http
+{
+
+ServerShould::~ServerShould() = default;
+
+}
+
+QTEST_MAIN(Http::ServerShould)
